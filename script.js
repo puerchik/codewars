ad9de495c9031b9f2f0bf5c1d15e6ce963f848d6
puerchik <rumyantsevnn@yandex.ru>
2023-09-09 16:51:07 +0300

    Ones and Zeros

--- script.js
+++ script.js
@@ -1,18 +1,45 @@
-const points = games => {
+const binaryArrayToNumber = arr => {
     let result = 0;
-    for (let i = 0; i < games.length; i++) {
-        Number(games[i].split(":")[0]) > Number(games[i].split(":")[1])
-            ? result += 3
-            : Number(games[i].split(":")[0]) < Number(games[i].split(":")[1])
-                ? result += 0
-                : result += 1
+
+    for (let i = 0; i < arr.length; i++) {
+        result = result * 2 + arr[i]
     }
 
     return result;
 }
 
+const binaryArrayToNumberSecondVersion = arr => arr.reduce((a, b) => 2 * a + b, 0)
 
-console.log(points(["1:0", "2:0", "3:0", "4:4", "2:2", "3:3", "1:4", "2:3", "2:4", "3:4"]));
+console.log(binaryArrayToNumber([1, 0, 1, 1, 0, 0, 1]));
+
+
+// Testing: [0, 0, 0, 1] ==> 1
+// Testing: [0, 0, 1, 0] ==> 2
+// Testing: [0, 1, 0, 1] ==> 5
+// Testing: [1, 0, 0, 1] ==> 9
+// Testing: [0, 0, 1, 0] ==> 2
+// Testing: [0, 1, 1, 0] ==> 6
+// Testing: [1, 1, 1, 1] ==> 15
+// Testing: [1, 0, 1, 1] ==> 11
+
+
+
+
+// const points = games => {
+//     let result = 0;
+//     for (let i = 0; i < games.length; i++) {
+//         Number(games[i].split(":")[0]) > Number(games[i].split(":")[1])
+//             ? result += 3
+//             : Number(games[i].split(":")[0]) < Number(games[i].split(":")[1])
+//                 ? result += 0
+//                 : result += 1
+//     }
+
+//     return result;
+// }
+
+
+// console.log(points(["1:0", "2:0", "3:0", "4:4", "2:2", "3:3", "1:4", "2:3", "2:4", "3:4"]));
 
 
 // if x > y: 3 points (win)
