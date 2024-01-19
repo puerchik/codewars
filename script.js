897d546b9210a420b3952d2195c356ea642fc8bf
puerchik <rumyantsevnn@yandex.ru>
2024-01-19 10:44:00 +0300

    Human Readable Time

--- script.js
+++ script.js
@@ -1,8 +1,32 @@
-const pigIt = str => str.split(' ').map(w => /[\.,\-\!\?]/.test(w) ? w : w.slice(1) + w.charAt(0) + 'ay').join(' ');
+const humanReadable = seconds => {
+    let h = Math.floor(seconds / 3600);
+    let m = Math.floor((seconds % 3600) / 60);
+    let s = seconds % 60;
+
+    return `${h > 9 ? h : '0' + h}:${m > 9 ? m : '0' + m}:${s > 9 ? s : '0' + s}`;
+}
+
+
+console.log(humanReadable(90));
+
+
+// strictEqual(humanReadable(0), '00:00:00', 'humanReadable(0)');
+// strictEqual(humanReadable(59), '00:00:59', 'humanReadable(59)');
+// strictEqual(humanReadable(60), '00:01:00', 'humanReadable(60)');
+// strictEqual(humanReadable(90), '00:01:30', 'humanReadable(90)');
+// strictEqual(humanReadable(3599), '00:59:59', 'humanReadable(3599)');
+// strictEqual(humanReadable(3600), '01:00:00', 'humanReadable(3600)');
+// strictEqual(humanReadable(45296), '12:34:56', 'humanReadable(45296)');
+// strictEqual(humanReadable(86399), '23:59:59', 'humanReadable(86399)');
+// strictEqual(humanReadable(86400), '24:00:00', 'humanReadable(86400)');
+// strictEqual(humanReadable(359999), '99:59:59', 'humanReadable(359999)');
+
+
+// const pigIt = str => str.split(' ').map(w => /[\.,\-\!\?]/.test(w) ? w : w.slice(1) + w.charAt(0) + 'ay').join(' ');
 
 
 
-console.log(pigIt('Hello world !'));
+// console.log(pigIt('Hello world !'));
 
 // pigIt('Pig latin is cool'); // igPay atinlay siay oolcay
 // pigIt('Hello world !');     // elloHay orldway !
