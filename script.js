5c56e956641fee9ae36b542f5f0cf76642284108
puerchik <rumyantsevnn@yandex.ru>
2024-03-08 18:09:00 +0300

    Quadrants

--- script.js
+++ script.js
@@ -1,6 +1,31 @@
-const quadratic = (x1, x2) => [1, -(x1 + x2), x1 * x2];
+const quadrant = (x, y) => {
+    if (x > 0 && y > 0) {
+        return 1;
+    }
+    if (x < 0 && y > 0) {
+        return 2;
+    }
+    if (x < 0 && y < 0) {
+        return 3;
+    }
+    if (x > 0 && y < 0) {
+        return 4;
+    }
+}
 
-console.log(quadratic(5, -4));
+console.log(quadrant(19, -56));
+
+// (1, 2)     => 1
+// (3, 5)     => 1
+// (-10, 100) => 2
+// (-1, -9)   => 3
+// (19, -56)  => 4
+
+
+
+// const quadratic = (x1, x2) => [1, -(x1 + x2), x1 * x2];
+
+// console.log(quadratic(5, -4));
 
 
 
