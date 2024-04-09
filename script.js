88902c3ee017723637a526bb6a00f4787c1ae502
puerchik <rumyantsevnn@yandex.ru>
2024-04-09 15:59:18 +0300

    Merging sorted integer arrays (without duplicates)

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const mergeArrays = (a, b) => [...new Set(a.concat(b))].sort((a, b) => a - b)
+const mergeArrays = (a, b) => [...new Set(a.concat(b))].sort((a, b) => a - b);
 
 console.log(mergeArrays([1, 3, 5], [2, 4, 6]));
 
