6175129bdf735f1a62ef9f950cb51de0478cd5c7
puerchik <rumyantsevnn@yandex.ru>
2024-04-09 15:58:52 +0300

    Merging sorted integer arrays (without duplicates)

--- script.js
+++ script.js
@@ -1,6 +1,25 @@
-const solution = str => str.split('').reverse().join('');
+const mergeArrays = (a, b) => [...new Set(a.concat(b))].sort((a, b) => a - b)
 
-console.log(solution('h'));
+console.log(mergeArrays([1, 3, 5], [2, 4, 6]));
+
+// assert.deepEqual(mergeArrays([1, 3, 5], [2, 4, 6]), [1, 2, 3, 4, 5, 6]);
+// assert.deepEqual(mergeArrays([2, 4, 8], [2, 4, 6]), [2, 4, 6, 8]);
+
+
+
+
+// assert.deepEqual(longestMountainPass([], 0), [0, 0]);
+// ssert.deepEqual(longestMountainPass([10, 10, 10], 0), [3, 0]);
+// assert.deepEqual(longestMountainPass([1, 2, 3, 4, 5], 0), [1, 0]);
+
+// assert.deepEqual(longestMountainPass([10, 9, 8, 7, 6, 5, 4, 3, 2, 3], 1), [10, 0]);
+// assert.deepEqual(longestMountainPass([9, 1, 2, 3, 4, 5, 6, 9], 7), [7, 0]);
+// assert.deepEqual(longestMountainPass([1, 8, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10], 7), [9, 1]);
+
+
+// const solution = str => str.split('').reverse().join('');
+
+// console.log(solution('h'));
 
 // assert.strictEqual(solution('world'), 'dlrow');
 // assert.strictEqual(solution('hello'), 'olleh');
