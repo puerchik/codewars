e0a4c517b2fc486477de2ebc9706b14a1e31e8af
puerchik <rumyantsevnn@yandex.ru>
2024-04-26 07:04:06 +0300

    No Loops 2 - You only need one

--- script.js
+++ script.js
@@ -1,6 +1,18 @@
-const printArray = array => array.join(",");
+const check = (a, x) => a.includes(x);
 
-console.log(printArray([2, 4, 5, 2]));
+console.log(check([80, 117, 115, 104, 45, 85, 112, 115], 45));
+
+//     assert.strictEqual(check([66, 101], 66), true);
+//     assert.strictEqual(check([80, 117, 115, 104, 45, 85, 112, 115], 45), true);
+//     assert.strictEqual(check(['t', 'e', 's', 't'], 'e'), true);
+//     assert.strictEqual(check(['what', 'a', 'great', 'kata'], 'kat'), false);
+
+
+
+
+// const printArray = array => array.join(",");
+
+// console.log(printArray([2, 4, 5, 2]));
 
 //  [2,4,5,2] - "2,4,5,2"
 
