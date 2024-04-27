32b37d735dc6a6494b819462783e6bcbb5f54f39
puerchik <rumyantsevnn@yandex.ru>
2024-04-27 07:17:08 +0300

    SpeedCode #2 - Array Madness

--- script.js
+++ script.js
@@ -1,6 +1,25 @@
-const check = (a, x) => a.includes(x);
+const arrayMadness = (a, b) => a.reduce((acc, c) => acc + Math.pow(c, 2), 0) > b.reduce((acc, c) => acc + Math.pow(c, 3), 0);
 
-console.log(check([80, 117, 115, 104, 45, 85, 112, 115], 45));
+console.log(arrayMadness([1, 2, 3, 4, 5], [2, 2, 2, 2, 2, 2, 1, 1, 1]));
+
+// Test.assertEquals(arrayMadness([4,5,6],[1,2,3]), true);
+// Test.assertEquals(arrayMadness([5,6,7],[4,5,6]), false);
+// Test.assertEquals(arrayMadness([4,5,6],[3,4,5]), false);
+// Test.assertEquals(arrayMadness([3,4,5],[2,3,4]), false);
+// Test.assertEquals(arrayMadness([2,3,4],[1,2,3]), false);
+// Test.assertEquals(arrayMadness([1,2,3],[0,1,2]), true);
+// Test.assertEquals(arrayMadness([5,3,2,4,1],[15]), false);
+// Test.assertEquals(arrayMadness([2,5,3,4,1],[3,3,3,3,3]), false);
+// Test.assertEquals(arrayMadness([1,3,5,2,4],[2,2,2,2,2,2,2,1]), false);
+// Test.assertEquals(arrayMadness([1,2,3,4,5],[2,2,2,2,2,2,1,1,1]), true);
+// Test.assertEquals(arrayMadness([2,4,6,8,10,12,14],[1,3,5,7,9,11,13]), false);
+
+
+
+
+// const check = (a, x) => a.includes(x);
+
+// console.log(check([80, 117, 115, 104, 45, 85, 112, 115], 45));
 
 //     assert.strictEqual(check([66, 101], 66), true);
 //     assert.strictEqual(check([80, 117, 115, 104, 45, 85, 112, 115], 45), true);
