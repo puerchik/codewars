77286da9a33990256400644ec8e52bb513bf21c3
puerchik <rumyantsevnn@yandex.ru>
2024-05-21 06:51:11 +0300

    Remove String Spaces

--- script.js
+++ script.js
@@ -1,13 +1,24 @@
-const removeChar = str => {
-    const strArr = str.split('');
+const noSpace = x => x.split('').filter(el => el !== ' ').join('');
 
-    strArr.pop();
-    strArr.shift();
+console.log(noSpace('8 j 8   mBliB8g  imjB8B8  jl  B'));
 
-    return strArr.join('');
-}
+// assert.strictEqual(noSpace('8 j 8   mBliB8g  imjB8B8  jl  B'), '8j8mBliB8gimjB8B8jlB');
+// assert.strictEqual(noSpace('8 8 Bi fk8h B 8 BB8B B B  B888 c hl8 BhB fd'), '88Bifk8hB8BB8BBBB888chl8BhBfd');
+// assert.strictEqual(noSpace('8aaaaa dddd r     '), '8aaaaaddddr');
 
-console.log(removeChar('eloquent'));
+
+
+
+// const removeChar = str => {
+//     const strArr = str.split('');
+
+//     strArr.pop();
+//     strArr.shift();
+
+//     return strArr.join('');
+// }
+
+// console.log(removeChar('eloquent'));
 
 // assert.strictEqual(removeChar('eloquent'), 'loquen');
 // assert.strictEqual(removeChar('country'), 'ountr');
