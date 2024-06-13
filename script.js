e9566578029992c858c31169b5dcec82a21d4721
puerchik <rumyantsevnn@yandex.ru>
2024-06-13 07:16:23 +0300

    Square Every Digit

--- script.js
+++ script.js
@@ -1,4 +1,27 @@
-const boolToWord = bool => bool ? "Yes" : "No"
+const squareDigits = num => +num.toString()
+    .split('')
+    .map(el => Math.pow(Number(el), 2))
+    .join('')
+
+console.log(squareDigits(3212))
+
+// it("squareDigits(3212) should equal 9414", () => {
+//     assert.strictEqual(squareDigits(3212), 9414);
+//   });
+
+//   it("squareDigits(2112) should equal 4114", () => {
+//     assert.strictEqual(squareDigits(2112), 4114);
+//   });
+
+//   it("squareDigits(0) should equal 0", () => {
+//     assert.strictEqual(squareDigits(0), 0);
+//   });
+
+
+
+
+
+// const boolToWord = bool => bool ? "Yes" : "No"
 
 
 
