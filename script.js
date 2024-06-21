4ae7ab8855a59a29b462668b29c272aa27556dbb
puerchik <rumyantsevnn@yandex.ru>
2024-06-21 07:28:19 +0300

    You're a square!

--- script.js
+++ script.js
@@ -1,6 +1,20 @@
-const filter_list = l => l.filter(el => typeof (el) === 'number')
+const isSquare = n => Number.isInteger(Math.sqrt(n))
 
-console.log(filter_list([1, 'a', 'b', 0, 15]));
+console.log(isSquare(25));
+
+// Test.assertEquals(isSquare(-1), false, "-1: Negative numbers cannot be square numbers");
+// Test.assertEquals(isSquare( 0), true, "0 is a square number (0 * 0)");
+// Test.assertEquals(isSquare( 3), false, "3 is not a square number");
+// Test.assertEquals(isSquare( 4), true, "4 is a square number (2 * 2)");
+// Test.assertEquals(isSquare(25), true, "25 is a square number (5 * 5)");
+// Test.assertEquals(isSquare(26), false, "26 is not a square number");
+
+
+
+
+// const filter_list = l => l.filter(el => typeof (el) === 'number')
+
+// console.log(filter_list([1, 'a', 'b', 0, 15]));
 
 // assert.deepEqual(filter_list([1, 2, 'a', 'b']), [1, 2], 'For input [1,2,"a","b"]');
 // assert.deepEqual(filter_list([1, 'a', 'b', 0, 15]), [1, 0, 15], 'For input [1,"a","b",0,15]');
