c60eed78acb9a2db4ae9b43adb684c1cce09bb2a
puerchik <rumyantsevnn@yandex.ru>
2024-06-29 07:28:47 +0300

    Mumbling

--- script.js
+++ script.js
@@ -1,16 +1,32 @@
-const getMiddle = s => {
-    const strLength = s.length
-    const halfLength = Math.floor(strLength / 2)
+const accum = s => s
+    .split('')
+    .map((el, i) => el.toUpperCase() + el.repeat(i).toLowerCase())
+    .join('-')
 
-    if (strLength % 2) {
-        return s.slice(halfLength, halfLength + 1)
-    } else {
-        return s.slice(halfLength - 1, halfLength + 1)
-    }
-}
+console.log(accum("ZpglnRxqenU"));
+
+// Test.assertEquals(accum("ZpglnRxqenU"), "Z-Pp-Ggg-Llll-Nnnnn-Rrrrrr-Xxxxxxx-Qqqqqqqq-Eeeeeeeee-Nnnnnnnnnn-Uuuuuuuuuuu");
+// Test.assertEquals(accum("NyffsGeyylB"), "N-Yy-Fff-Ffff-Sssss-Gggggg-Eeeeeee-Yyyyyyyy-Yyyyyyyyy-Llllllllll-Bbbbbbbbbbb");
+// Test.assertEquals(accum("MjtkuBovqrU"), "M-Jj-Ttt-Kkkk-Uuuuu-Bbbbbb-Ooooooo-Vvvvvvvv-Qqqqqqqqq-Rrrrrrrrrr-Uuuuuuuuuuu");
+// Test.assertEquals(accum("EvidjUnokmM"), "E-Vv-Iii-Dddd-Jjjjj-Uuuuuu-Nnnnnnn-Oooooooo-Kkkkkkkkk-Mmmmmmmmmm-Mmmmmmmmmmm");
+// Test.assertEquals(accum("HbideVbxncC"), "H-Bb-Iii-Dddd-Eeeee-Vvvvvv-Bbbbbbb-Xxxxxxxx-Nnnnnnnnn-Cccccccccc-Ccccccccccc");
 
 
-console.log(getMiddle("middle"));
+
+
+// const getMiddle = s => {
+//     const strLength = s.length
+//     const halfLength = Math.floor(strLength / 2)
+
+//     if (strLength % 2) {
+//         return s.slice(halfLength, halfLength + 1)
+//     } else {
+//         return s.slice(halfLength - 1, halfLength + 1)
+//     }
+// }
+
+
+// console.log(getMiddle("middle"));
 
 // Test.assertEquals(getMiddle("test"), "es");
 // Test.assertEquals(getMiddle("testing"), "t");
